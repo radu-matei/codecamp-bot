575f9ac10dc30f8b40cd9bd6a9f332800fc658cd
radu-matei <matei.radu94@gmail.com>
2017-11-22 15:20:50 +0200

    Update text
    Signed-off-by: radu-matei <matei.radu94@gmail.com>

--- bot/app.ts
+++ bot/app.ts
@@ -31,7 +31,7 @@ bot.dialog('GetInformation', function (session) {
         path: '/get/cluster'
     };
 
-    var body = "";
+    var body = "Hello Cluj!";
     http.get(options, response => {
         response.on('data', data => {
             body += data
